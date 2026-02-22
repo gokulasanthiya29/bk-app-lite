3ab0c3489640195f0498db780e1ae97c46b7a664
Gokula Santhiya <santhiyasandyrc@gmail.com>
2026-02-22 22:22:05 +0530

    Create index.test.js

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const assert = require('node:assert');
+const { greet } = require('../index.js');
+
+assert.strictEqual(greet(), 'Hello, World!');
+assert.strictEqual(greet('Buildkite'), 'Hello, Buildkite!');
+
+console.log('All tests passed.');
